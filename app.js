a47f52bca14f749da8f3a79d56fc346a9bb15c95
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-02-18 17:17:02 +0900

    라우터 설정, url에 맞는 템플릿 파일 생성

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var router = require('./router/main');
 
 var app = express();
 
+// 라우터 적용
+app.use('/', router);
+
+// ejs엔진 적용
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// ejs로 렌더링할것!
+app.engine('html', require('ejs').renderFile);
+
 var server = app.listen(3000, function () {
   console.log("Express server has started on port 3000")
-})
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
 })
