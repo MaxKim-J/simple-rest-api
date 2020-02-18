67137e9ceb55090907cbc25c479d1258c0b58b42
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-02-18 18:00:14 +0900

    미들웨어 사용해보기

--- app.js
+++ app.js
@@ -6,32 +6,33 @@ var fs = require("fs")
 
 var app = express();
 
+// 미들웨어 #1 - 로컬호스트 url로 요청왔을때의 응답
+app.use(function (req, res, next) {
+  console.log('첫번째 미들웨어에서 요청을 처리함');
+  req.user = "max";
+  // 처리 순서 넘겨주기
+  next();
+})
 
-// ejs엔진 적용
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// 미들웨어 #2 - 로컬호스트 url로 요청왔을때의 응답
+app.use(function (req, res, next) {
+  console.log('두번째 미들웨어에서 요청을 처리함');
 
-// ejs로 렌더링할것!
-app.engine('html', require('ejs').renderFile);
+  // 보낼 데이터 명시하고 end로 미들웨어 끝내주면서 응답 보내기
+  res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+  res.end('<h1>' + req.user + '의 요청이군!</h1>')
 
-var server = app.listen(3000, function () {
+  // 요러케 하면 json을 데이터를 보냄
+  //todo 요청객체, 응답객체 정리
+  // res.send({ name: 'max', age: 20 })
+})
+
+// 로컬호스트 포트 지정
+// set으로 express 객체(app) 안에 설정한 속성은 get으로 뽑아서 쓸 수 이따
+//todo 메소드 정리
+
+app.set('port', process.env.PORT || 3000)
+var server = app.listen(app.get('port'), function () {
   console.log("Express server has started on port 3000")
 })
 
-app.use(express.static('public'));
-
-// 세션 모듈만 써도 직접 쿠키에 접근할 수 있어서 cookie-parser은 이제 필요 없음
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(session({
-  // 쿠키 임의 변조를 막기 위한 sign값
-  secret: '@#@$MYSIGN#@$#$',
-  // 세션을 언제나 저장할 것인지 정하는 값
-  resave: false,
-  // 새로 생겼지만 변경되지 않는 세션
-  saveUninitialized: true
-}));
-
-// 라우터 적용
-// bodyparser 밑에 있어야 함
-app.use('/', router);
